f219ca2d6ea6a7946e4bfd351d666b282a1bbfaa
Bryand Acosta <b_acosta@icloud.com>
2018-05-12 22:33:00 -0400

    Change for eslint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
-    Item = require('./api/models/itemModel'), //created model loading here
+    Item = require('./API/models/itemModel'), //created model loading here
     bodyParser = require('body-parser');
   
 // mongoose instance connection url connection
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://localhost/shinigaminglifedb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/itemRoutes'); //importing route
+var routes = require('./API/routes/itemRoutes'); //importing route
 routes(app); //register the route
 
 app.listen(port);
